00574ccb146c1117a9ee77054a38144700e2aab2
jgliner <jarrett.gliner@gmail.com>
2016-01-20 19:01:20 -0800

    Implemented code for single rook solution

--- src/solvers.js
+++ src/solvers.js
@@ -17,40 +17,15 @@
 
 window.findNRooksSolution = function(n) {
   // var solution = undefined; //fixme
-
   var board = new Board({n:n});
-  board.get(0)[0] = 1;
-  function addRows(board, row, hasConflict) {
-    var matrix =[];
+  var matrix =[];
     for (var i=0;i<board.get('n');i++){
      matrix.push(board.get(i));
     }
-    var countFilledRows;
-   
-    if (row >= board.get('n')) return matrix;
-    var found = false;
-    if(hasConflict){
-      var column = board.get(row).indexOf(1) + 1;
-    }
-    var column = column || 0;
-    for(column=0;column< board.get('n');column++){
-      board.get(row)[column] = 1;
-      var rowConflict = board.hasAnyRowConflicts();
-      var colConflict = board.hasAnyColConflicts();
-      if (!rowConflict && !colConflict){
-        found = true;
-        return addRows(board, row+1);
-      }
-    }
-    if(!found){
-      return addRows(board, row-1, true);
-    } 
+  for(var i=0;i<matrix.length;i++){
+    matrix[i][i] =1;
   }
-
-  return addRows(board, 0, false);
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  // return solution;
+  return matrix;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
