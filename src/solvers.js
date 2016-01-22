53a8dac9911b98a34ee5aa786457307af986c400
jgliner <jarrett.gliner@gmail.com>
2016-01-21 20:55:11 -0800

    Implement working solver

--- src/solvers.js
+++ src/solvers.js
@@ -16,8 +16,15 @@
 
 
 window.findNRooksSolution = function(n) {
-  // var solution = undefined; //fixme
-
+  var board = new Board({n:n});
+  var matrix =[];
+  for (var i=0;i<board.get('n');i++){
+    matrix.push(board.get(i));
+  }
+  for(var j=0;j<matrix.length;j++){
+    matrix[j][j] = 1;
+  }
+  return matrix;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
@@ -54,10 +61,31 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({n:n});
+  var x;
+  var counter = 0;
+  var findSolution = function(row) {
+    if (row === n) {
+      x = _.map(board.rows(), function(row) {
+        return row.slice();
+      });
+      return;
+    }
+    for (var j= 0; j < n; j++) {
+      board.togglePiece(row, j);
+      if (!board.hasAnyMajorDiagonalConflicts() && !board.hasAnyMinorDiagonalConflicts() && !board.hasAnyRowConflicts() && !board.hasAnyColConflicts()) { 
+        if(row === n){
+          console.log(board.rows());
+        }
+        findSolution(row+1);
+      }
+      board.togglePiece(row, j);
+    }
+  };
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  //console.log(findSolution(0));
+  findSolution(0);
+  return x || board.rows();
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
@@ -77,7 +105,7 @@ window.countNQueensSolutions = function(n) {
       // place
       board.togglePiece(row, i);
       // check for collision
-      if (!board.hasAnyQueensConflicts()) {
+      if (!board.hasAnyMajorDiagonalConflicts() && !board.hasAnyMinorDiagonalConflicts() && !board.hasAnyRowConflicts() && !board.hasAnyColConflicts()) {
         findSolutions(row+1);
       }
       // if collision
